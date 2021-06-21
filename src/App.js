798369cc4e26115aca9d5a0806751a7116b0751d
glaze <luyiping1011@gmail.com>
2021-06-20 22:53:21 -0700

    add signature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+/* eslint-disable */
 import './App.css';
+import { ethers } from "ethers";
+import { useState } from 'react';
+const Web3 = require('web3')
 
 function App() {
+  // const [provider, setProvider] = useState(new ethers.providers.Web3Provider(window.ethereum));
+  const [message, setMessage] = useState('NA');
+  const [to, setTo] = useState('');
+  const [tokenId, setTokenId] = useState('');
+  const [messageHash, setMessageHash] = useState('NA');
+
+  const changeMessageHash = () => {
+    var web3 = new Web3(Web3.givenProvider || "ws://localhost:8546");
+    setMessageHash(web3.utils.soliditySha3(to, tokenId));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <form>
+        <label>
+          address to
+          <input type="text" value={to} onChange={(event) => { setTo(event.target.value) }} />
+        </label>
+
+        <label>
+          tokenId
+          <input type="text" value={tokenId} onChange={(event) => { setTokenId(event.target.value) }} />
+        </label>
+
+      </form>
+      <button onClick={changeMessageHash}>Hash</button>
+      <p>Hash:{messageHash}</p>
+
+
+      <button onClick={async () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const signedMessage = await signer.signMessage(ethers.utils.arrayify(MessageHash))
+        setMessage(signedMessage)
+      }
+      }>Sign NFT Issue</button>
+
+      <p>Signature: {message}</p>
+    </div >
   );
 }
 
